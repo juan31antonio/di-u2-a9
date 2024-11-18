892452c155dff52aa7797dd6e8d3d3df7c354cc2
juan31antonio <juanantoniocorder32@gmail.com>
2024-11-18 11:52:23 +0100

    finalizado

--- src/app/processQueue.js
+++ src/app/processQueue.js
@@ -1,7 +1,14 @@
 export function getFinalState(baseState, queue) {
     let finalState = baseState;
   
-    // TODO: haz algo con la cola...
+    for(var i = 0;i < queue.length;i++){
+      if(typeof queue[i] === 'function'){
+        finalState = queue[i](finalState)
+      }
+      else{
+        finalState = queue[i];
+      }
+    }
   
     return finalState;
   }
